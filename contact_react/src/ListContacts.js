d84b018a1d2488a7e0904d8ed80b5e8edfd2d174
Dhamija <vd250095@ncr.com>
2018-05-02 16:24:57 +0530

    Use componentDidMount to fetch contacts from server and Remove contact from database when deleted in app

--- contact_react/src/ListContacts.js
+++ contact_react/src/ListContacts.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import PropTypes from 'prop-types';
+import escapeRegExp from 'escape-string-regexp';
+import sortBy from 'sort-by';
 
 class ListContacts extends Component{
 static propTypes = {
@@ -15,16 +17,45 @@ updateQuery = (query) => {
     this.setState({query: query.trim()})
 }
 
+clearQuery = () => {
+    this.setState({query : ''})
+}
+
     render() {
+        const {contacts, onDeleteContact} = this.props
+        const {query} = this.state
+
+        let showingContacts
+        if(query){
+            const match = new RegExp(escapeRegExp(this.state.query), 'i')
+            showingContacts = contacts.filter((conatct) => match.test(conatct.name))
+        }
+        else{
+            showingContacts = contacts            
+        }
+
+        showingContacts.sort(sortBy('name'))
         return (
 
         <div className="list-contacts">
         <div className="list-contacts-top">
-        <input className="search-contacts" type="text" placeholder="Search contacts" value={this.state.query} 
-        onChange={(event) => this.state.updateQuery(event.target.value)} />
+        <input 
+        className="search-contacts" 
+        type="text" 
+        value={this.state.query} 
+        onChange={event => this.updateQuery(event.target.value)}
+        placeholder="Search contacts"
+         />
             </div>    
+            {showingContacts.length !== contacts.length && (
+                <div className="showing-contacts">
+                <span>Now Showing {showingContacts.length} of {contacts.length} total</span>
+                <button onClick={this.clearQuery}>Show all</button>
+                </div>
+            )}
+
             <ol className="contact-list">
-            {this.props.contacts.map((contact) => (
+            {showingContacts.map((contact) => (
                 <li key={contact.id} className="contact-list-item">
                     <div className="contact-avatar" style={{
                         backgroundImage: `url(${contact.avatarURL})`
@@ -35,7 +66,7 @@ updateQuery = (query) => {
                     <p>{contact.email}</p>
                     </div>
     
-                    <button onClick={() => this.props.onDeleteContact(contact)} className="contact-remove">
+                    <button onClick={() => onDeleteContact(contact)} className="contact-remove">
                         Remove
                     </button>
                 </li>
